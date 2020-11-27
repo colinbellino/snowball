ddb88511835783b46bdadeb7cdc28133cb1e33e5
Colin Bellino <bellino.colin@gmail.com>
2020-11-27 01:30:16 +0100

    ✨ Add cheats and color projectiles

--- Assets/Code/SecretSanta.Game/GameConfig.cs
+++ Assets/Code/SecretSanta.Game/GameConfig.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Secret Santa/Config")]
 public class GameConfig : ScriptableObject
 {
+	[Header("Cheats")]
+	public bool CheatsFullTeam = false;
+
+	[Header("Data")]
 	public Recruit[] Recruits;
 	public Level[] Levels;
+	public string[] PlaceholderNames = { "Anri", "Rena", "Riku", "Andre", "Thea", "Mariel", "Jesse", "Marceline", "Gaius", "Ursula", "Madeline", "Theo", "Régis" };
+
+	[Header("Prefabs")]
 	public Entity EnemyPrefab;
 	public Entity RecruitPrefab;
+
 	public readonly Vector2 AreaSize = new Vector2(32, 18);
-	public string[] PlaceholderNames = { "Anri", "Rena", "Riku", "Andre", "Thea", "Mariel", "Jesse", "Marceline", "Gaius", "Ursula", "Madeline", "Theo", "Régis" };
 }
--- Assets/Code/SecretSanta.Game/Weapon.cs
+++ Assets/Code/SecretSanta.Game/Weapon.cs
@@ -22,6 +22,7 @@ public class Weapon : MonoBehaviour
 
 		var projectileEntity = Instantiate(_projectilePrefab, _projectileOrigin.position, _projectileOrigin.rotation);
 		projectileEntity.Alliance.SetCurrent(_entity.Alliance.Current);
+		projectileEntity.Renderer.Color = _entity.Renderer.Color;
 		OnFired?.Invoke(projectileEntity);
 
 		_cooldownTimestamp = Time.time + projectileEntity.Projectile.Cooldown;
