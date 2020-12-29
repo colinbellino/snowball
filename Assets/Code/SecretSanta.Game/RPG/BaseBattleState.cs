266bdeb175ebced7170f4c0c4a5067b816a9ee31
Colin Bellino <bellino.colin@gmail.com>
2020-12-29 03:05:51 +0100

    ✨ Add quick and dirty battle menu

--- Assets/Code/SecretSanta.Game/RPG/BaseBattleState.cs
+++ Assets/Code/SecretSanta.Game/RPG/BaseBattleState.cs
@@ -12,6 +12,7 @@ namespace Code.SecretSanta.Game.RPG
 		protected readonly GameControls _controls;
 		protected readonly BattleStateMachine _machine;
 		protected readonly Camera _camera;
+		protected readonly BattleUI _ui;
 
 		protected List<UnitComponent> _allUnits => _battle.Units;
 		protected Turn _turn => _battle.Turn;
@@ -24,6 +25,7 @@ namespace Code.SecretSanta.Game.RPG
 			_config = Game.Instance.Config;
 			_controls = Game.Instance.Controls;
 			_camera = Game.Instance.Camera;
+			_ui = Game.Instance.BattleUI;
 		}
 	}
 }
