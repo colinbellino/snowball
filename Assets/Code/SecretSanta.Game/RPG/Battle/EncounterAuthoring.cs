9d21a63a73794002feee6b4930523bdd3760bd4b
Colin Bellino <bellino.colin@gmail.com>
2021-01-05 23:38:08 +0100

    🔧 Sync scriptableObjects filename with the Name property (in editor)

--- Assets/Code/SecretSanta.Game/RPG/Battle/EncounterAuthoring.cs
+++ Assets/Code/SecretSanta.Game/RPG/Battle/EncounterAuthoring.cs
@@ -1,13 +1,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace Code.SecretSanta.Game.RPG
 {
 	[CreateAssetMenu(menuName = "Secret Santa/RPG/Encounter")]
-	public class EncounterAuthoring: ScriptableObject
+	public class EncounterAuthoring : ScriptableObject
 	{
 		public int Id;
+		public string Name;
 		public Area Area;
 		public List<UnitAuthoring> Foes;
+
+#if UNITY_EDITOR
+		private void OnValidate()
+		{
+			if (name != Name)
+			{
+				var assetPath = AssetDatabase.GetAssetPath(this.GetInstanceID());
+				AssetDatabase.RenameAsset(assetPath, Name);
+				AssetDatabase.SaveAssets();
+			}
+		}
+#endif
 	}
 }
