44a1f9af67e9fd6439a7aac863d89d10f39c1116
Colin Bellino <bellino.colin@gmail.com>
2021-01-05 23:38:08 +0100

    🔧 Sync scriptableObjects filename with the Name property (in editor)

--- Assets/Code/SecretSanta.Game/RPG/Unit/UnitAuthoring.cs
+++ Assets/Code/SecretSanta.Game/RPG/Unit/UnitAuthoring.cs
@@ -1,21 +1,30 @@
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace Code.SecretSanta.Game.RPG
 {
 	[CreateAssetMenu(menuName = "Secret Santa/RPG/Unit")]
 	public class UnitAuthoring : ScriptableObject
 	{
 		public int Id;
-		[SerializeField] private string _name;
+		public string Name;
 		public Color Color = Color.magenta;
 		public int MoveRange = 3;
 		public int Health = 1;
 
-		public string Name => _name;
-
+#if UNITY_EDITOR
 		private void OnValidate()
 		{
-			_name = name;
+			if (name != Name)
+			{
+				var assetPath = AssetDatabase.GetAssetPath(this.GetInstanceID());
+				AssetDatabase.RenameAsset(assetPath, Name);
+				AssetDatabase.SaveAssets();
+			}
 		}
+#endif
 	}
 }
