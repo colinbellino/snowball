936ca4d2937984ff8ceb5c999697a4686bf7e53b
Colin Bellino <bellino.colin@gmail.com>
2021-01-06 00:05:26 +0100

    🐛 Turn in direction if needed when moving

--- Assets/Code/SecretSanta.Game/RPG/Battle/PerformMoveState.cs
+++ Assets/Code/SecretSanta.Game/RPG/Battle/PerformMoveState.cs
@@ -8,16 +8,9 @@ namespace Code.SecretSanta.Game.RPG
 
 		public async Task Enter(object[] args)
 		{
-			var destination = _turn.MovePath[_turn.MovePath.Count - 1];
-			var direction = destination - _turn.InitialPosition;
-
-			if (direction.y == 0 && direction.x != 0f && direction.x != _turn.Unit.Direction.x)
-			{
-				await _turn.Unit.Facade.ChangeDirection(direction);
-				_turn.Unit.Direction = direction;
-			}
-
 			await _turn.Unit.Facade.MoveOnPath(_turn.MovePath);
+			await _turn.Unit.Facade.ChangeDirection(_turn.Unit.Direction.x);
+
 			_turn.Unit.GridPosition = _turn.MovePath[_turn.MovePath.Count - 1];
 			_turn.HasMoved = true;
 
