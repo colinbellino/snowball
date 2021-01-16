59c8609b7ce0c00ed11ebb36528a9da90e12b599
Colin Bellino <bellino.colin@gmail.com>
2021-01-16 20:12:16 +0100

    ✨ Prettier worldmap

--- Assets/Code/Snowball.Game/RPG/Worldmap/WorldmapEncounterPoint.cs
+++ Assets/Code/Snowball.Game/RPG/Worldmap/WorldmapEncounterPoint.cs
@@ -7,22 +7,18 @@ namespace Snowball.Game
 	public class WorldmapEncounterPoint : MonoBehaviour, IPointerClickHandler
 	{
 		[SerializeField] private SpriteRenderer _spriteRenderer;
-		private bool _enabled;
 
 		public event Action Clicked;
 
 		public void OnPointerClick(PointerEventData eventData)
 		{
-			if (_enabled)
-			{
 				Clicked?.Invoke();
-			}
 		}
 
-		public void SetEnabled(bool enabled)
+		public void SetCleared(bool cleared)
 		{
-			_enabled = enabled;
-			_spriteRenderer.color = _enabled ? new Color(0.4f, 0.8f, 0.2f, 1f) : Color.gray;
+			var color = cleared ? new Color(0.2f, 0.6f, 0.2f, 1f) : new Color(0.8f, 0.2f, 0.2f, 1f);
+			_spriteRenderer.material.SetColor("ReplacementColor1", color);
 		}
 	}
 }
