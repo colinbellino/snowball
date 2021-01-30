199f9baf0d3786b76e115d97f2680cf4771f95bd
Colin Bellino <bellino.colin@gmail.com>
2021-01-30 15:43:11 +0100

    🐛 Add Version to game save so we can migrate stuff when needed

--- Assets/Code/Snowball.Game/RPG/DebugUI.cs
+++ Assets/Code/Snowball.Game/RPG/DebugUI.cs
@@ -4,14 +4,6 @@ namespace Snowball.Game
 {
 	public class DebugUI : MonoBehaviour
 	{
-		public void Save()
-		{
-			SaveHelpers.SaveToFile(Game.Instance.State);
-		}
 
-		public void Load()
-		{
-			Game.Instance.State.Update(SaveHelpers.LoadFromFile());
-		}
 	}
 }
--- Assets/Code/Snowball.Game/RPG/Game/GameState.cs
+++ Assets/Code/Snowball.Game/RPG/Game/GameState.cs
@@ -5,6 +5,7 @@ namespace Snowball.Game
 	// This is what will be persisted when saving the game
 	public class GameState
 	{
+		public string Version;
 		public int CurrentEncounterId;
 		public List<int> EncountersDone;
 		public List<int> Party;
