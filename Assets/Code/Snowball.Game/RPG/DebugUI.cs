169e208150ef36b5c74e46f7b38041a814b90318
Colin Bellino <bellino.colin@gmail.com>
2021-01-30 15:52:56 +0100

    🔧 Display build version in UI

--- Assets/Code/Snowball.Game/RPG/DebugUI.cs
+++ Assets/Code/Snowball.Game/RPG/DebugUI.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Snowball.Game
 {
 	public class DebugUI : MonoBehaviour
 	{
+		[SerializeField] private Text _versionText;
 
+		private void Awake()
+		{
+			_versionText.text = $"v{Application.version}";
+		}
 	}
 }
